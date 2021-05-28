934c49af649dcba4384b7d75c967822382f9eee5
safutrasetya <safutrasetya@gmail.com>
2021-05-29 06:52:40 +0700

    5/29/2021 - HALUTAMA dah ada

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('halutama');
+});
+Route::get('/halutama', function() {
+  return view('halutama');
+});
+Route::get('/test', function(){
+  return view('test');
 });
