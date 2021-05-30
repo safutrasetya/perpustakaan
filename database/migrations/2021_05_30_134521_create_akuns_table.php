c190510013d5f4d195e71e467532e3e51ec04611
Febri Naomi Sofia Tampubolon <naomisofiatampubolon@gmail.com>
2021-05-31 06:42:13 +0700

    update migration dan model untuk table akuns, 31/05/2021

---
+++ database/migrations/2021_05_30_134521_create_akuns_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAkunsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('akuns', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            // $table->unsignedint ('id');
+            $table->string ('username', 20);
+            $table->string ('email');
+            $table->string ('password', 16);
+            // $table->('level');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('akuns');
+    }
+}
