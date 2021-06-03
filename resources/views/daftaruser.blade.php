2ddfe2b14fde867a6898ff2839cbe61c5cb741ca
fadil0711 <outher.heaven07@gmail.com>
2021-06-03 12:44:50 +0700

    6/3/21 halprofiladmin

---
+++ resources/views/daftaruser.blade.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+
+    <style type="text/css">
+    .table{
+        font-family: sans-serif;
+        color: #444;
+        border-collapse: collapse;
+        border: 1px solid #f2f5f7;
+        margin-top: 50px;
+        text-align: center;
+    }
+
+    .table tr th{
+        background: rgb(38, 224, 137);
+        color: #fff;
+        font-weight: normal;
+    }
+
+    .table tr:nth-child(even){
+        background: rgb(145, 141, 141);
+    }
+
+
+    </style>
+
+  </head>
+  <body>
+
+
+    <div class="container">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Username</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Password</th>
+                    <th scope="col">Level</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td scope="row"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td scope="row"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td scope="row"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  </body>
+</html>
