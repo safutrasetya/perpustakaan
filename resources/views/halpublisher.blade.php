bf215e51cdea91bd331fbaa83f405359bce11b8a
safutrasetya <safutrasetya@gmail.com>
2021-06-03 12:07:28 +0700

    6/3/2021 UPDATE halpublisher

--- resources/views/halpublisher.blade.php
+++ resources/views/halpublisher.blade.php
@@ -1,72 +1,43 @@
-@extends('layout.v_navbarhalpublisher')
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-
-    <style type="text/css">
-    .table{
-        font-family: sans-serif;
-        color: rgb(68, 68, 68);
-        border-collapse: collapse;
-        border: 1px solid #f2f5f7;
-        margin-top: 50px;
-        text-align: center;
-    }
-
-    .table tr th{
-        background: rgb(247, 152, 74);
-        color: #fff;
-        font-weight: normal;
-    }
-
-    .table tr:nth-child(even){
-        background: rgb(145, 141, 141);
-    }
-
-
-    </style>
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <!--BOOTSTRAP JQUERY-->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    <!--END BOOTSTRAP JQUERY-->
+    <title></title>
   </head>
-  
   <body>
-
-
-    <div class="container">
-        <table class="table table-bordered">
+    <div class="jumbotron bg-dark text-center text-white" style="margin-bottom:0">
+      <div class="container">
+        <p class="display-1">DAFTAR BUKUKU</p>
+      </div>
+    </div>
+    <div class="jumbotron bg-info">
+      <div class="container pt-5">
+        <table class="table table-bordered table-secondary table-striped">
             <thead>
                 <tr>
-                    <th scope="col">Cover</th>
-                    <th scope="col">Judul</th>
-                    <th scope="col">Penerbit</th>
-                    <th scope="col">Pengarang</th>
-                    <th scope="col">Tahun Terbit</th>
-                    <th scope="col">Sinopsis</th>
-                    <th scope="col">Tersedia ditoko:</th>
-                    <th scope="col">Action</th>
+                    <th>Cover</th>
+                    <th>Judul</th>
+                    <th>Penerbit</th>
+                    <th>Pengarang</th>
+                    <th>Tahun Terbit</th>
+                    <th>Sinopsis</th>
+                    <th>Tersedia ditoko:</th>
+                    <th>Action</th>
                 </tr>
             </thead>
 
             <tbody>
                 <tr>
-                    <td scope="row"></td>
                     <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td scope="row"></td>
                     <td></td>
                     <td></td>
                     <td></td>
@@ -76,57 +47,7 @@
                     <td></td>
                 </tr>
                 <tr>
-                    <td scope="row"></td>
                     <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td scope="row"></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td scope="row"></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td scope="row"></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td scope="row"></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td scope="row"></td>
                     <td></td>
                     <td></td>
                     <td></td>
@@ -138,16 +59,6 @@
             </tbody>
         </table>
     </div>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
-    -->
+  </div>
   </body>
 </html>
